e193b187067151689be573e2ab7e73d611ba6c64
Yatharth <72456920+Yatharthg12@users.noreply.github.com>
2025-12-18 01:09:54 +0530

    Create FinancialAdvisor.java

---
+++ FinancialAdvisor.java
@@ -0,0 +1,69 @@
+package com.wellsfargo.entities;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+public class FinancialAdvisor {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String firstName;
+
+    @Column(nullable = false)
+    private String lastName;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @OneToMany(mappedBy = "financialAdvisor", cascade = CascadeType.ALL)
+    private List<Client> clients;
+
+    public FinancialAdvisor() {}
+
+    public FinancialAdvisor(String firstName, String lastName, String email, List<Client> clients) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.clients = clients;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public List<Client> getClients() {
+        return clients;
+    }
+
+    public void setClients(List<Client> clients) {
+        this.clients = clients;
+    }
+}
